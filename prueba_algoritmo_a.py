f46e830fa7786b3374244b558b784fe3bad88cd0
JavierTorresO <javitorres2022@udec.cl>
2025-09-25 09:48:11 -0300

    Se añade timer para las comparaciones de tiempo

--- prueba_algoritmo_a.py
+++ prueba_algoritmo_a.py
@@ -1,9 +1,10 @@
 from laberinto import Laberinto
 from agente import Agente
 from algoritmo_a_estrella import a_estrella_con_llave  
+import time
 
 # Crear laberinto y agente
-lab = Laberinto(10, prob_muro=0.5)
+lab = Laberinto(12, prob_muro=0.4)
 lab.colocar_inicio(0, 0)
 lab.generar_salidas_aleatorias(min_salidas=2, max_salidas=5)
 lab.colocar_llave()  
@@ -12,7 +13,9 @@ agente = Agente(lab)
 print("Laberinto inicial:")
 agente.mostrar_laberinto()
 
+inicio=time.time()
 # Ejecutar A* considerando la llave y las salidas falsas
 a_estrella_con_llave(lab, agente, delay=0.5)
-
+fin=time.time()
+print(f"Tiempo de ejecución: {fin - inicio:.4f} segundos")
 # si está demorando mucho cortar la terminal a la fuerza: ctrl+c
