be2f9d1ad08a71dd84ca7ba4fa1598d0e66803f2
JavierTorresO <javitorres2022@udec.cl>
2025-09-24 23:50:47 -0300

    Se hace el juego infinito, ahora si no hay camino posible hasta un objetivo el agente se movera hacia los lados esperando que los muros se muevan aleatoriamente y se pueda generar un camino posible. Solo se podria acabar el juego si queda rodeado por los 4 lados por muros

--- algoritmo_a_estrella.py
+++ algoritmo_a_estrella.py
@@ -1,5 +1,6 @@
 from queue import PriorityQueue
 import time
+import random
 
 def heuristica(fila, columna):
     # distancia Manhattan como heuristica
@@ -49,55 +50,76 @@ def a_estrella(laberinto, inicio, objetivo):
     return None  # no se encontro camino
 
 
+def mover_aleatoriamente(laberinto, agente, delay=0.5):
+    # mueve al Agente a una posicion vecina  aleatoriamente (que no sea muro)...
+    f, c = agente.posicion
+    movimientos = [(-1,0),(1,0),(0,-1),(0,1)]
+    random.shuffle(movimientos)
+    for df, dc in movimientos:
+        nf, nc = f + df, c + dc
+        if 0 <= nf < laberinto.n and 0 <= nc < laberinto.n and laberinto.grid[nf][nc] != laberinto.MUROS:
+            agente.posicion = (nf, nc)
+            agente.verificar_llave_y_salida()
+            laberinto.mover_muros()
+            agente.mostrar_laberinto()
+            time.sleep(delay)
+            return True # siesque se movio a una posicion vecina al azar
+    return False  # siesque no habian movimientos posibles
+
+
 def a_estrella_con_llave(laberinto, agente, delay=0.3):
     """
     Se cambio a un 'A* incremental'
-    -con cada paso que hace el agente se recalcula el camino->antes al calcular el camino desde el inicio daba problemas porque el mapa se iba modificando(muros moviles) y podia dar problemas.
-        Sentiamos que el agente 'no aprendía'
-    -no servian de nada las salidas falsas si le damos la salida_real para calcular el camino, entonces una vez llega a la llave va a probar todas las salidas por cercania desde ese punto hasta llegar a la real
+    -con cada paso que hace el agente se recalcula el camino
+    -al recoger la llave se van a probar las salidas desde la más cercana hasta la más lejana del punto donde se recogio la llave hasta encontrar la real
+    -ULTIMO AÑADIDO: juego infinito, si da la casualidad de que no haya ningun camino para llegar a la llave/salida, el agente se va a mover los lados hasta que de la probabilidad de mover los muros y que se libere un camino para llegar al objetivo
+                    unico bloqueo posible: si al moverse el Agente se mueven los muros y lo dejan rodeado de muros y no pueda moverse a ningun lado
     """
 
     # 1. Ir a la llave
     while not agente.tiene_llave:
-        # recalcular el camino desde la posición actual hasta la llave
         camino = a_estrella(laberinto, agente.posicion, laberinto.llave)
         if camino is None:
-            print("No se puede llegar a la llave. Laberinto bloqueado.")
-            return
-        
-        # moverse paso a paso (solo un paso por iteración)
+            pudo_mover = mover_aleatoriamente(laberinto, agente, delay)
+            if not pudo_mover:
+                print("El Agente fue rodeado por los muros, no se puede mover.\nFIN DEL JUEGO.")
+                return
+            else:
+                print("No se observa ningún camino posible hasta la llave... Caminaré un poco por si se mueven los muros...\n")
+            continue
+
         siguiente_paso = camino[1]
         agente.posicion = siguiente_paso
+        agente.verificar_llave_y_salida()
         laberinto.mover_muros()
-        agente.verificar_llave_y_salida()  # recoger llave si corresponde
         agente.mostrar_laberinto()
         time.sleep(delay)
 
     # 2. Ir a las salidas, de la más cercana a la más lejana
-    salidas_ordenadas = sorted(laberinto.salidas,
-                               key=lambda x: heuristica(agente.posicion, x))
-    
+    salidas_ordenadas = sorted(laberinto.salidas, key=lambda x: heuristica(agente.posicion, x))
     salida_encontrada = False
+
     for salida in salidas_ordenadas:
         while agente.posicion != salida:
-            # recalcular camino paso a paso desde la posición actual hasta esta salida
             camino = a_estrella(laberinto, agente.posicion, salida)
             if camino is None:
-                print(f"No se puede llegar a la salida {salida}. Laberinto bloqueado.")
-                break
+                pudo_mover = mover_aleatoriamente(laberinto, agente, delay)
+                if not pudo_mover:
+                    print("El Agente fue rodeado por los muros, no se puede mover.\nFIN DEL JUEGO.")
+                    return
+                else:
+                    print("No se observa ningún camino posible hasta la siguiente salida... Caminaré un poco por si se mueven los muros...\n")
+                continue
 
-            # moverse paso a paso
             siguiente_paso = camino[1]
             agente.posicion = siguiente_paso
-            laberinto.mover_muros()
-            # verificar si es salida y si tiene llave
             if agente.verificar_llave_y_salida():
                 salida_encontrada = True
                 break
-
+            laberinto.mover_muros()
             agente.mostrar_laberinto()
             time.sleep(delay)
-        
+
         if salida_encontrada:
             break
 
--- prueba_algoritmo_a.py
+++ prueba_algoritmo_a.py
@@ -1,10 +1,9 @@
-import time
 from laberinto import Laberinto
 from agente import Agente
 from algoritmo_a_estrella import a_estrella_con_llave  
 
 # Crear laberinto y agente
-lab = Laberinto(10, prob_muro=0.3)
+lab = Laberinto(10, prob_muro=0.5)
 lab.colocar_inicio(0, 0)
 lab.generar_salidas_aleatorias(min_salidas=2, max_salidas=5)
 lab.colocar_llave()  
@@ -15,3 +14,5 @@ agente.mostrar_laberinto()
 
 # Ejecutar A* considerando la llave y las salidas falsas
 a_estrella_con_llave(lab, agente, delay=0.5)
+
+# si está demorando mucho cortar la terminal a la fuerza: ctrl+c
